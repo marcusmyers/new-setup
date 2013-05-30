12824cdfe709ad7d4bb0c3f93b9bfbdd06027ab1
marcusmyers <marcusmyers@gmail.com>
2013-05-30 16:02:01 -0400

    Made a few changes to the nanorc clone

--- setup.sh
+++ setup.sh
@@ -4,10 +4,14 @@
 # This file must be ran as root
 #
 
+# useful unicode chars:
+#✓ \xe2\x9c\x93
+#✗ \xe2\x9c\x97
+
 # Make sure XCode Command Line Tools are installed
 # before we do anything else - exit if not
 if ! type -p gcc > /dev/null; then
-    echo "You Do NOT have XCode Command Line Tools installed"
+    echo -e "\xe2\x9c\x97 You Do NOT have XCode Command Line Tools installed"
     exit 1
 fi
 
@@ -30,10 +34,25 @@ if ! type -p brew > /dev/null; then
     brew install git ruby-build rbenv nodejs
 fi
 
+# Install bash completion for Mac OS X
+brew install bash-completion
+echo "Add the following lines to your ~/.bash_profile:"
+echo "  if [ -f $(brew --prefix)/etc/bash_completion ]; then"
+echo "    . $(brew --prefix)/etc/bash_completion"
+echo "  fi"
+
 
 # Clone Nano Syntax Highlighting Files
 if [ ! -d "~/.nano" ]; then
-    git clone https://github.com/marcusmyers/nanorc.git
+    git clone https://github.com/marcusmyers/nanorc.git ./.nano
     ln -s ./.nano ../.nano
     ln -s ./.nanorc ../.nanorc
+fi
+
+
+
+
+# Link .bash_ps1 file
+if [ ! -f "~/.bash_ps1" ]; then
+    ln -s ./.bash_ps1 ../.bash_ps1
 fi
