43739d00e8401f9c8c5fef893d2a3ea87bc8dc22
Mark Myers <marcusmyers@gmail.com>
2015-02-06 06:57:46 -0500

    Updated for new bash profile stuff and aliases

--- .bash_profile
+++ .bash_profile
@@ -2,34 +2,20 @@
 export PATH="/usr/local/mysql/bin:$PATH"
 export PATH=/usr/local/php5/bin:$PATH
 
+# some settings to be more colorful
+export GREP_OPTIONS='--color=auto' GREP_COLOR='1;32'
+export CLICOLOR=true
+export LSCOLORS=ExGxFxdxCxDxDxBxBxExEx
+
 # use homebrew to install bash_completion
 if [ -f $(brew --prefix)/etc/bash_completion ]; then
   . $(brew --prefix)/etc/bash_completion
 fi
 
-
-#### Aliases ####
-
-# Common
-alias c='clear'
-
-# Git
-alias g='git status'
-alias gca='git commit -am'
-alias gco='git checkout'
-alias gp='git pull'
-alias gpu='git push'
-
-# PHP Artisan
-alias pa='php artisan'
-alias pagm='php artisan generate:model'
-alias pagr='php artisan generate:resource'
-alias pagc='php artisan generate:controller'
-alias pagmi='php artisan generate:migration'
-alias pags='php artisan generate:seed'
-
-#### End Aliases ####
-
+## Add .aliases to profile
+if [ -f "$HOME/.aliases" ]; then
+  . "$HOME/.aliases"
+fi
 
 
 ## CDPATH is one of bashs best "secret" features. eg. "cd projects" will drop
@@ -42,3 +28,50 @@ CDPATH=.:~:~/Documents:~/Documents/Projects:~/Dropbox:/Volumes
 if [ -f "$HOME/.bash_ps1" ]; then
 . "$HOME/.bash_ps1"
 fi
+
+
+################################################
+# bash functions
+################################################
+function myip {
+  res=$(curl -s checkip.dyndns.org | grep -Eo '[0-9\.]+')
+  echo "$res"
+}
+
+function git_stats {
+# awesome work from https://github.com/esc/git-stats
+# including some modifications
+if [ -n "$(git symbolic-ref HEAD 2> /dev/null)" ]; then
+    echo "Number of commits per author:"
+    git --no-pager shortlog -sn --all
+    AUTHORS=$( git shortlog -sn --all | cut -f2 | cut -f1 -d' ')
+    LOGOPTS=""
+    if [ "$1" == '-w' ]; then
+        LOGOPTS="$LOGOPTS -w"
+        shift
+    fi
+    if [ "$1" == '-M' ]; then
+        LOGOPTS="$LOGOPTS -M"
+        shift
+    fi
+    if [ "$1" == '-C' ]; then
+        LOGOPTS="$LOGOPTS -C --find-copies-harder"
+        shift
+    fi
+    for a in $AUTHORS
+    do
+        echo '-------------------'
+        echo "Statistics for: $a"
+        echo -n "Number of files changed: "
+        git log $LOGOPTS --all --numstat --format="%n" --author=$a | cut -f3 | sort -iu | wc -l
+        echo -n "Number of lines added: "
+        git log $LOGOPTS --all --numstat --format="%n" --author=$a | cut -f1 | awk '{s+=$1} END {print s}'
+        echo -n "Number of lines deleted: "
+        git log $LOGOPTS --all --numstat --format="%n" --author=$a | cut -f2 | awk '{s+=$1} END {print s}'
+        echo -n "Number of merges: "
+        git log $LOGOPTS --all --merges --author=$a | grep -c '^commit'
+    done
+else
+    echo "you're currently not in a git repository"
+fi
+}
--- setup.sh
+++ setup.sh
@@ -8,30 +8,47 @@
 #✓ \xe2\x9c\x93
 #✗ \xe2\x9c\x97
 
+#--------------------------------------------------------------------
+# Modifiable variables, please set them via environmental variables.
+#--------------------------------------------------------------------
+NODE_VERSION=${NODE_VERSION:-"v0.10.36"}
+NODE_PACKAGE_URL=${FACTER_PACKAGE_URL:-"http://nodejs.org/dist/${NODE_VERSION}/node-${NODE_VERSION}.pkg"}
+
 # Make sure XCode Command Line Tools are installed
-# before we do anything else - exit if not
+# before we do anything else - install if not
 if ! type -p gcc > /dev/null; then
-    echo -e "\xe2\x9c\x97 You Do NOT have XCode Command Line Tools installed"
-    exit 1
+    echo -e "\xe2\x9c\x97 You Do NOT have XCode Command Line Tools installed.  Installing now"
+    xcode-select --install
 fi
 
 # Check if PHP is installed
 if ! type -p php > /dev/null; then
-    curl -s http://php-osx.liip.ch/install.sh | bash -s 5.4
+    echo -e "\xe2\x9c\x97 PHP is not installed.  Installing now"
+    curl -s http://php-osx.liip.ch/install.sh | bash -s 5.6
+    echo -e "\xe2\x9c\x93 PHP is installed"
 fi
 
 # Check if Composer is installed
 if ! type -p composer > /dev/null; then
+    echo "### INSTALLING Composer ###"
     curl -sS https://getcomposer.org/installer | php
-    mv composer.phar /usr/local/bin/composer
+    sudo mv composer.phar /usr/local/bin/composer
+  
 fi
 
+
+# Check if node.js is installed
+if ! type -p node > /dev/null; then
+  curl -O ${NODE_PACKAGE_URL}
+  installer -pkg node-${NODE_VERSION}.pkg -target / >/dev/null  
+fi
 
 # Check if Homebrew is installed and then install all
 # ruby pre-requisites 
 if ! type -p brew > /dev/null; then
     ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
-    brew install git ruby-build rbenv nodejs
+    brew tap homebrew/dupes
+    brew install git nano 
 fi
 
 # Install bash completion for Mac OS X
@@ -44,8 +61,34 @@ echo "  fi"
 
 # Clone Nano Syntax Highlighting Files
 if [ ! -d "~/.nano" ]; then
-    git clone https://github.com/marcusmyers/nanorc.git ./.nano
-    ln -s ./.nano ../.nano
-    ln -s ./.nanorc ../.nanorc
+    git clone https://github.com/marcusmyers/nanorc.git ~/.nano
+#    ln -s ./.nano ../.nano
+#    ln -s ./.nanorc ../.nanorc
+    cp ./.nanorc ~/.nanorc
 fi
 
+echo "### INSTALLING GEMS ###"
+echo "--- Installing librarian-puppet gem..."
+gem install librarian-puppet
+echo -e "\xe2\x9c\x93 librarian-puppet is installed"
+echo "--- Installing beaker gem..."
+gem install beaker
+echo -e "\xe2\x9c\x93 beaker is installed"
+echo "--- Installing tugboat gem..."
+gem install tugboat
+echo -e "\xe2\x9c\x93 tugboat is installed"
+echo "--- Installing rspec-puppet gem..."
+gem install rspec-puppet
+echo -e "\xe2\x9c\x93 rspec-puppet is installed"
+echo "--- Installing puppet-blacksmith gem..."
+gem install puppet-blacksmith
+echo -e "\xe2\x9c\x93 puppet-blacksmith is installed"
+echo "--- Installing rake gem..."
+gem install rake
+echo -e "\xe2\x9c\x93 rake is installed"
+echo "--- Installing bundler gem..."
+gem install bundler
+echo -e "\xe2\x9c\x93 bundler is installed"
+echo "--- Installing aws-sdk gem..."
+gem install aws-sdk
+echo -e "\xe2\x9c\x93 aws-sdk is installed"
