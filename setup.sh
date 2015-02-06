be4571cab6f8532c3002807c0847c6cf8121fd2a
Mark Myers <marcusmyers@gmail.com>
2015-02-06 12:12:17 -0500

    Test for liip.ch php directory

--- setup.sh
+++ setup.sh
@@ -62,11 +62,11 @@ if ! type -p gcc > /dev/null; then
 fi
 
 # Check if PHP is installed
-#if ! type -p php > /dev/null; then
+if [ ! -d /usr/local/php5 ]; then
     echo -e "\xe2\x9c\x97 Installing latest version of php now"
     curl -s http://php-osx.liip.ch/install.sh | bash -s 5.6
     echo -e "\xe2\x9c\x93 PHP is installed"
-#fi
+fi
 
 # Check if Composer is installed
 if ! type -p composer > /dev/null; then
